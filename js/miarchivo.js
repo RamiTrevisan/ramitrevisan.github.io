e9aafd3d7038a325f471fa51639036b4410e71a6
Ramiro Trevisan <113390325+RamiTrevisan@users.noreply.github.com>
2024-08-26 12:00:04 -0300

    Imprimir

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -96,6 +96,7 @@ function eliminarFavorito(fecha, id) {
 }
 
 // Función para imprimir la tabla
+// Función para imprimir la tabla
 function printTable() {
   // Abre una nueva ventana para imprimir la tabla
   var printWindow = window.open("", "", "height=600,width=800");
@@ -107,6 +108,7 @@ function printTable() {
                 <tr>
                     <th>FECHA</th>
                     <th>MONEDA</th>
+                    <th>ID</th>
                     <th>COMPRA</th>
                     <th>VENTA</th>
                 </tr>
@@ -114,24 +116,26 @@ function printTable() {
             <tbody>
     `;
 
-  // Agrega las filas de datos dinámicamente
-  document.querySelectorAll("#container-content .fecha-header")
-    document.forEach((fechaHeader) => {
-      const fecha = fechaHeader.textContent;
-      content += `<tr><td colspan="5" style="font-weight:bold;">${fecha}</td></tr>`;
+  // Selecciona todos los elementos con la clase "fecha-header"
+  const fechas = document.querySelectorAll("#container-content .fecha-header");
 
-      let nextSibling = fechaHeader.nextElementSibling;
-      while (nextSibling && !nextSibling.classList.contains("fecha-header")) {
-        if (nextSibling.classList.contains("element")) {
-          content += "<tr>";
-          nextSibling.querySelectorAll("div:not(.accion)").forEach((cell) => {
-            content += `<td>${cell.innerHTML}</td>`;
-          });
-          content += "</tr>";
-        }
-        nextSibling = nextSibling.nextElementSibling;
+  // Agrega las filas de datos dinámicamente
+  fechas.forEach((fechaHeader) => {
+    const fecha = fechaHeader.textContent;
+    content += `<tr><td colspan="5" style="font-weight:bold;">${fecha}</td></tr>`;
+
+    let nextSibling = fechaHeader.nextElementSibling;
+    while (nextSibling && !nextSibling.classList.contains("fecha-header")) {
+      if (nextSibling.classList.contains("element")) {
+        content += "<tr>";
+        nextSibling.querySelectorAll("div:not(.accion)").forEach((cell) => {
+          content += `<td>${cell.innerHTML}</td>`;
+        });
+        content += "</tr>";
       }
-    });
+      nextSibling = nextSibling.nextElementSibling;
+    }
+  });
 
   content += `
             </tbody>
